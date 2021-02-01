b02c24f2d22d076fa79e6adba83b8689f9f0d5df
JGilbe28-7 <jonathan.gilbert@mac.com>
2021-01-31 20:01:37 -0500

    updated the contact.js start - fix the .ready error

---
+++ public/js/contact.js
@@ -0,0 +1,11 @@
+//https://api.jquery.com/ready/ - 
+//The $(document).ready(function) .ready crossed out
+ // Handler for .ready() called.
+$(function() {
+  var contactForm = $(".contact");
+  var firstNameInput = $("#firstName-input");
+  var lastNameInput = $("#lastName-input");
+  var emailInput = $("#email-input");
+  var reasonInput = $("#reason-input");
+  
+});
